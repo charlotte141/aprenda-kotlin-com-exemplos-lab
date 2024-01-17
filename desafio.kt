e594919df756cc8145a27cbd584991459308e596
Delcio_Santos <delcoluisgabi@gmail.com>
2024-01-17 23:30:08 +0100

    desafio concluido

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,69 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { INICIANTE, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+ 
+data class Usuario (
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+    val name: String, 
+    val email: String, 
+    val age: Int, 
+    var Telefone: String // variavel do tipo String para poder armazenar os codigos telefonico de cada paiz ou estado
+    
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+ 
+data class ConteudoEducacional(
+    
+    val nome: String, 
+    val duracao: Int = 120
+    
+)
+
+  
+data class Formacao(
+    
+    val nome: String, 
+    val nivel: Nivel, 
+    val conteudos: List<ConteudoEducacional>
+    
+) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+        
+		// inscrição de 1 ou mais usuarios ao mesmo tempo
+        for (dadosDoUsuario in usuario){
+            inscritos.add(dadosDoUsuario)
+            println("usuario ${dadosDoUsuario.name} incrito")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    //armazenamento temporario da lista de conteudos educacional 
+    val conteudosEducacional = listOf<ConteudoEducacional>(
+    	ConteudoEducacional("SQL", 180),
+        ConteudoEducacional("NoSQL", 180),
+        ConteudoEducacional("funções em Kotlin")
+    )
+    
+    //definição do curso para inscrição 
+    val formacao = Formacao(
+    	"Desenvolvimento Backend", Nivel.INTERMEDIARIO, conteudosEducacional
+    )
+    
+    //armazenamento temporario da lista de Usuarios
+    val usuario = listOf<Usuario>(
+    	Usuario("Danilo", "danilsonGeril90@gmail", 14, "+244 951027824"),
+        Usuario("Elfio", "gabiel145Tutty@gmail", 20, "+244 945980096"),
+        Usuario("Elidiu", "chamielCigueira@gmail", 32, "+244 967234099")
+    )
+    
+	
+    formacao.matricular(usuario[0], usuario[1])
+    formacao.matricular(usuario[2])
+
 }
